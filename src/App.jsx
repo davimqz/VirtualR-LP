b117b367ccdec0827296650fc5a959b1b66eb4dc
Davi Marques <dmo3@cesar.school>
2025-07-01 06:28:11 -0300

    [Init] - Base struct

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+const App = () => {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline justify-center text-center p-6">Hello world!</h1>
+    </>
+  )
+}
+
+export default App
