2e3493337efc7bc1e86a0315db1bd4d692ad690f
Davi Marques <dmo3@cesar.school>
2025-07-01 09:36:27 -0300

    [Feat] - Add workflow-section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './index.css'
 import Navbar from './components/NavBar'
 import HeroSection from './components/HeroSection'
 import FeatureSection from './components/Feature'
-
+import Workflow from './components/Workflow'
 const App = () => {
   return (
     <>
@@ -10,6 +10,7 @@ const App = () => {
         <div className="max-w-7xl mx-auto pt-20 px-6">
           <HeroSection></HeroSection>
           <FeatureSection></FeatureSection>
+          <Workflow></Workflow>
       </div>
     </>
   )
