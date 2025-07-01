f1badd1e74380d0aac0768c97bbd33e2fc74cae5
Davi Marques <dmo3@cesar.school>
2025-07-01 08:52:39 -0300

    [Feat] - Add feature-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './index.css'
 import Navbar from './components/NavBar'
 import HeroSection from './components/HeroSection'
+import FeatureSection from './components/Feature'
 
 const App = () => {
   return (
     <>
       <Navbar></Navbar>
-      <HeroSection></HeroSection>
+        <div className="max-w-7xl mx-auto pt-20 px-6">
+          <HeroSection></HeroSection>
+          <FeatureSection></FeatureSection>
+      </div>
     </>
   )
 }
